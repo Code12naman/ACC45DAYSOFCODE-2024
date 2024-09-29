58122fbe7378074a321d0e594b1c8fe38f5233fe
Code12naman <144878132+Code12naman@users.noreply.github.com>
2024-09-29 18:53:32 +0530

    Add files via upload

---
+++ day2_TCS_EXAMINATION.py
@@ -0,0 +1,28 @@
+def find_winner(dragon_scores, sloth_scores):
+    # Calculate total scores for both
+    dragon_total = sum(dragon_scores)
+    sloth_total = sum(sloth_scores)
+    
+    if dragon_total > sloth_total:
+        return "Dragon"
+    elif sloth_total > dragon_total:
+        return "Sloth"
+    
+    if dragon_scores[0] > sloth_scores[0]:
+        return "Dragon"
+    elif sloth_scores[0] > dragon_scores[0]:
+        return "Sloth"
+    
+    if dragon_scores[1] > sloth_scores[1]:
+        return "Dragon"
+    elif sloth_scores[1] > dragon_scores[1]:
+        return "Sloth"
+    
+    return "Tie"
+
+T = int(input())  
+for _ in range(T):
+    dragon_scores = list(map(int, input().split()))  
+    sloth_scores = list(map(int, input().split()))  
+    result = find_winner(dragon_scores, sloth_scores)
+    print(result)
