a7b3da2e0c92566c5d5ba689f757eb928104fc1c
Code12naman <144878132+Code12naman@users.noreply.github.com>
2024-10-29 20:25:50 +0530

    Add files via upload

---
+++ DAY 27 PRB01.py
@@ -0,0 +1,29 @@
+# cook your dish here
+def is_prime(n):
+    if n <= 1:
+        return False
+    if n <= 3:
+        return True
+    if n % 2 == 0 or n % 3 == 0:
+        return False
+    i = 5
+    while i * i <= n:
+        if n % i == 0 or n % (i + 2) == 0:
+            return False
+        i += 6
+    return True
+
+def main():
+    T = int(input())
+    results = []
+    for _ in range(T):
+        N = int(input())
+        if is_prime(N):
+            results.append("yes")
+        else:
+            results.append("no")
+    
+    print("\n".join(results))
+
+if __name__ == "__main__":
+    main()
